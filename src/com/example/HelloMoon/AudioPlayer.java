946745ddbff2a14063ba5698867121c063cb49c3
GruzdovAN <antologyz@gmail.com>
2016-04-28 17:07:02 +0300

    add to git

---
+++ src/com/example/HelloMoon/AudioPlayer.java
@@ -0,0 +1,29 @@
+package com.example.HelloMoon;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+
+/**
+ * Created by Elkin on 27.04.2016.
+ */
+public class AudioPlayer {
+    private MediaPlayer mPlayer;
+    public void stop() {
+        if (mPlayer != null) {
+            mPlayer.release();
+            mPlayer = null;
+        }
+    }
+
+    public void play(Context context) {
+        stop();
+        mPlayer = MediaPlayer.create(context, R.raw.one_small_step);
+        mPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stop();
+            }
+        });
+        mPlayer.start();
+    }
+}
---
+++ src/com/example/HelloMoon/HelloMoonActivity.java
@@ -0,0 +1,15 @@
+package com.example.HelloMoon;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by Elkin on 27.04.2016.
+ */
+public class HelloMoonActivity extends FragmentActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_moon);
+    }
+}
